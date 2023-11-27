8c6c19925548e184f232f09b66074bb6377db1a6
Vycheslav Russu <vycheslavrussu@gmail.com>
2023-11-27 21:07:57 +0500

    За этот код мне придется замаливать грехи

--- practical-exercise-2/task-6.py
+++ practical-exercise-2/task-6.py
@@ -1,36 +1,67 @@
-input_string = 'Кот нос ток сон клад рама вход книга вдох нос'
+import copy
+
+input_string = input()
 input_string = input_string.casefold()
+
 words_list = input_string.split()
 
 words_dict = dict()
-letter_list = list()
+letter_set = set()
+letters_set = set()
+set_list = list()
+id_list = list()
+answer_list = list()
 
 for word in words_list:
-    if word == words_dict:
-        None
-    else:
-        for letter in word:
-            letter_list.append(letter)
-        words_dict[word] = letter_list.copy()
-        letter_list.clear()
+    for letter in word:
+        letters_set.add(letter)
+    set_list.append(letters_set.copy())
+    letters_set.clear()
 
-for first_word in words_dict:
-        if words_dict[first_word] == words_dict[first_word]:
-            print('yes')
+for i in range(0, len(words_list)):
+    for j in range(0, len(words_list)):
+        if set_list[i] == set_list[j]:
+            id_list.append(i)
+            id_list.append(j)
+            answer_list.append(id_list.copy())
+        id_list.clear()
 
-print(words_dict)
+answer = list()
 
+for couple in answer_list:
+    if couple[0] != couple[1]:
+        answer.append(couple)
 
+reversed_answer = copy.deepcopy(answer)
 
+a = int()
+b = int()
 
-# if {'g', 'w'} == {'w', 'g'}:
-#      print('Yes')
-# else:
-#     print('No')
+for couple in reversed_answer:
+    a = copy.deepcopy(couple[0])
+    b = copy.deepcopy(couple[1])
+    couple[0] = copy.deepcopy(b)
+    couple[1] = copy.deepcopy(a)
 
-# for i in range(0, len(inputString)):
-#     print(inputString[i])
-#     # reversedString += inputString[i]
+finaly_answer = list()
 
+for i in range(len(answer)):
+    if reversed_answer[i] not in finaly_answer:
+        finaly_answer.append(answer[i])
 
-# print(len(inputString))
+text_answer = list()
+temp_list = list()
+
+for i in range(len(finaly_answer)):
+    temp_list.append(words_list[finaly_answer[i][0]])
+    temp_list.append(words_list[finaly_answer[i][1]])
+    text_answer.append(temp_list.copy())
+    temp_list.clear()
+
+for couple in text_answer:
+    couple.sort()
+
+text_answer.sort()
+
+for couple in text_answer:
+    print(couple[0], couple[1])
